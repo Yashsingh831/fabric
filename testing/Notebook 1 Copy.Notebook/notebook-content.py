b3fbda94123cd45734d47a3d064acad3fb2ad52d
Yash pratap singh <74710703+Yashsingh831@users.noreply.github.com>
2025-08-14 22:57:26 +0530

    I'm committing all my changes.

---
+++ testing/Notebook 1 Copy.Notebook/notebook-content.py
@@ -0,0 +1,36 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+import matplotlib.pyplot as plt
+
+labels = 'S1', 'S2', 'S3'
+sections = [56, 66, 24]
+colors = ['c', 'g', 'y']
+
+plt.pie(sections, labels=labels, colors=colors,
+        startangle=90,
+        explode = (0, 0.1, 0),
+        autopct = '%1.2f%%')
+
+plt.axis('equal') # Try commenting this out.
+plt.title('Pie Chart Example')
+plt.show()
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
